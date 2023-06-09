5d9cb4645f55e7ba04f991dce8af05dd51205937
farzad <farzad.ghlm8@gmail.com>
2023-06-09 09:54:47 -0600

    complete binary search

--- data_search.py
+++ data_search.py
@@ -5,7 +5,19 @@ def linear_search(numbers, target):
     return -1
 
 def binary_search(numbers, target):
+    low = 0
+    high = len(numbers) - 1
 
+    while low <= high:
+        mid = (low + high) // 2
+        if numbers[mid] == target:
+            return mid
+        elif numbers[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
 
 
 
