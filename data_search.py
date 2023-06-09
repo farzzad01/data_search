ac8074fbc35f15a20c4890a31a2479e6261aaa2e
farzad <farzad.ghlm8@gmail.com>
2023-06-09 09:54:27 -0600

    first commit

---
+++ data_search.py
@@ -0,0 +1,13 @@
+def linear_search(numbers, target):
+    for i, num in enumerate(numbers):
+        if num == target:
+            return i
+    return -1
+
+def binary_search(numbers, target):
+
+
+
+
+    
+        
